a873eef3dca4af323eb9593495803e5d7ff6e581
myanch200 <78023091+myanch200@users.noreply.github.com>
2021-03-23 09:52:41 +0000

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -5,14 +5,14 @@ let amountInput = document.getElementById("amount");
 let yearlyBox = document.getElementById("yearly-box");
 let monthlyBox = document.getElementById("monthly-box");
 let weeklyBox = document.getElementById("weekly-box");
-
+let hourlyBox = document.getElementById("hourly-box");
+let hoursPerWeek = document.getElementById("amountHours");
 submitButton.addEventListener("click", calculate);
 
 let monthlyAmount = 0;
 let weeklyAmount = 0;
 let yearlyAmount = 0;
 let hourlyAmount = 0;
-
 function calculate(event) {
     event.preventDefault();
     let salaryType = document.querySelector('input[name="period"]:checked').value;
@@ -20,17 +20,30 @@ function calculate(event) {
         case "yearly":
             yearlyAmount = amountInput.value;
             monthlyAmount = yearlyAmount / 12;
-            hourlyAmount = monthlyAmount / 4;
+            weeklyAmount = monthlyAmount / 4;
+            hourlyAmount = weeklyAmount / hoursPerWeek.value;
+
             break;
         case "weekly":
             weeklyAmount = amountInput.value;
             monthlyAmount = weeklyAmount * 4;
             yearlyAmount = monthlyAmount * 12;
+            hourlyAmount = weeklyAmount / hoursPerWeek.value;
+
             break;
         case "monthly":
             monthlyAmount = amountInput.value;
             yearlyAmount = monthlyAmount * 12;
             weeklyAmount = monthlyAmount / 4;
+            hourlyAmount = weeklyAmount / hoursPerWeek.value;
+
+            break;
+        case "hourly":
+            hourlyAmount = amountInput.value;
+            weeklyAmount = hourlyAmount * hoursPerWeek.value;
+            monthlyAmount = weeklyAmount * 4;
+            yearlyAmount = monthlyAmount * 12;
+            console.log(`${hourlyAmount} - ${weeklyAmount} - ${monthlyAmount} - ${yearlyAmount}`);
             break;
         default:
             monthlyAmount = 0;
@@ -40,10 +53,10 @@ function calculate(event) {
 
 
     }
-
     yearlyBox.innerText = yearlyAmount;
     monthlyBox.innerText = monthlyAmount;
     weeklyBox.innerText = weeklyAmount;
+    hourlyBox.innerText = hourlyAmount;
 }
 
 function calculateTheSalary(salaryType) {
