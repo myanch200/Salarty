9eae232c15e95c9aeac3ce75a6cfc6bef539c9fb
myanch200 <myanch200@gmail.com>
2021-04-07 18:32:20 +0100

    Update: Job information page now fetches date from a api

--- job_information.js
+++ job_information.js
@@ -2,18 +2,18 @@ const form = document.querySelector('#jobForm');
 const textField = document.querySelector('#jobText');
 const jobSection = document.querySelector('.job-post-section');
 const btnSubmit = document.getElementById('btnSubmit');
-form.addEventListener('submit',listJobs);
-
+btnSubmit.addEventListener('click',listJobs);
 
 function listJobs(event){
     event.preventDefault();
-
+    jobSection.innerHTML = '';
     let baseUrl = 'http://api.lmiforall.org.uk/api/v1/soc/search?q=';
     let url = baseUrl +textField.value;
     console.log(url);
     fetch(url)
     .then(response => response.json())
     .then((data) => {
+
             console.log(data);
             data.forEach((item) =>{
                 let divItem = document.createElement('div');
@@ -22,13 +22,25 @@ function listJobs(event){
                 itemTitle.innerText = item.add_titles[0];
                 let itemDescirption = document.createElement('p');
                 itemDescirption.innerText = item.description;
+                let qualifications = document.createElement('p');
+                qualifications.innerText = item.qualifications;
+             
 
                 let itemSoc = document.createElement('em');
                 itemSoc.innerText = `Soc: ${item.soc}`;
 
+                let itemDetail = document.createElement('button')
+                itemDetail.className = 'item-detail-btn';
+                itemDetail.innerText = 'Details';
+
                 divItem.appendChild(itemTitle);
                 divItem.appendChild(itemDescirption);
                 divItem.appendChild(itemSoc);
+
+                divItem.appendChild(qualifications)
+
+                divItem.appendChild(itemDetail);
+
                 jobSection.appendChild(divItem);
 
 
