c052f8ee8eb8fe16465f4e38f246c7b0f6757f40
myanch200 <myanch200@gmail.com>
2021-04-08 16:34:04 +0100

    Update: modal showing recent pay information

--- job_information.js
+++ job_information.js
@@ -3,11 +3,67 @@ const textField = document.querySelector('#jobText');
 const jobSection = document.querySelector('.job-post-section');
 const btnSubmit = document.getElementById('btnSubmit');
 const modalSection = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
 
-
+modalSection.style.display = 'none';
 
 btnSubmit.addEventListener('click',listJobs);
+/**
+ * The idea to add event listener to document 
+ * and check if the target element is one of the buttons 
+ * rendered comes from this stackoverflow question 
+ * https://stackoverflow.com/questions/30601620/adding-an-event-listener-to-an-element-that-doesnt-exist-yet-in-vanilla-javascr
+ * the answer is posted from Satpal
+ */
+document.addEventListener('click',showModal);
 
+function showModal(e){
+    let element = e.target
+    if(element.tagName.toLowerCase() ==='button' && element.classList.contains('item-detail-btn')){
+            modalSection.style.display = 'block';
+           
+            
+            let socUrl ='http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc='
+            let searchUrl =socUrl+element.getAttribute('data-soc');
+            fetch(searchUrl).
+            then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let series =  data.series;
+                let mostRecent = 1800;
+                let currentSerie = null;
+                series.forEach((serie)=>{
+                   if(serie.year >= mostRecent){
+                       mostRecent = serie.year
+                       currentSerie = serie;
+                   }
+                })
+               
+                let weeklySalary = currentSerie.estpay;
+                let yearly = weeklySalary*52;
+                let infoP = document.createElement('p');
+                infoP.innerText = `Weekly: ${weeklySalary} - Yearly: ${yearly}`;
+                let closeBtn = document.createElement('span');
+                closeBtn.innerHTML ='&times;';
+                closeBtn.className ='close';
+                closeBtn.addEventListener("click",function(){
+                    modalSection.style.display = 'none';
+        
+                    });
+                modalContent.innerHTML = '';
+                modalContent.appendChild(closeBtn);
+                modalContent.appendChild(infoP);
+                
+            })
+
+
+
+
+    } 
+}
+
+
+///
 
 function listJobs(event){
     event.preventDefault();
@@ -44,6 +100,8 @@ function listJobs(event){
                 itemDetail.className = 'item-detail-btn';
                 itemDetail.innerText = 'Details';
                 itemDetail.setAttribute('data-soc',item.soc);
+                itemDetail.setAttribute('data-toggle','modal');
+                itemDetail.id
                 
                 divItem.appendChild(itemTitle);
                 divItem.appendChild(itemDescirption);
